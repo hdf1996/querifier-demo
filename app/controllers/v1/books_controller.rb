2d925c6b4b95189f2491feefe67828c6efea31e9
Hugoi <hi@hugo.farji.me>
2022-08-17 11:49:27 -0300

    Upgraded dependencies and added some comments

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -5,6 +5,9 @@ class V1::BooksController < ApplicationController
 
   private
 
+  # This could've been done with gems like Kaminari or anything that works
+  # with an ActiveRecord relation. 
+  # We just wanted to keep this repository low on external dependencies
   def paginate(collection)
     collection.limit(20).offset(page * 20)
   end
